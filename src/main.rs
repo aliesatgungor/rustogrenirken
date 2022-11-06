2346033a0e0f96a3b1b89ef677231779b7f885dc
aliesatgungor <aliesatgungor@outlook.com.tr>
2022-11-06 12:46:27 +0300

    Guess number but there is no conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use rand::Rng;
 fn main() {
     println!("Hello, world!");
+    let sn = rand::thread_rng().gen_range(1..=100);
+    println!("Number is {sn}");
+    println!("Type Number");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed To read line");
+
+    println!("You guessed {guess}");         
+
+
+
 }
